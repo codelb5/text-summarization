99b4922561e534404bf730326e8248625dad4138
bhaarathi05 <lakshmi.bhaarathi@unshots.com>
2025-01-07 18:32:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("Readme.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_NAME = "lakshmanbharathi"
+AUTHOR_USER_NAME = "codelb05"
+AUTHOR_EMAIL = "lakshmanbharathiofcl@gmail.com"
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python Text Summarizer package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
